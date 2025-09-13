285ef05b83c87062b4d570826a48195b5ff73790
kebab <shcmalivan@gmail.com>
2025-09-13 13:13:48 +0300

    Initial commit

---
+++ src/App.kt
@@ -0,0 +1,155 @@
+const val PI = 3.1415
+
+fun main()
+{
+
+//    println("Hello World!")
+//    print("Kotlin\n")
+//    println("Hello Kotlin!")
+//    println("yugyfg67uy6f7t6hihiwudhfiuhihdifhiwuf")
+/*
+    val city: String = "Volgograd"
+    println(city)
+    var num: Int = 2
+    num = 3
+    */
+    /*
+    val name = "Pasha"
+    var age = 2007
+    var age1 = 18
+    println(name)
+    println(age)
+    println(age1)
+    age1 = age1+10
+    println(age1)
+    */
+    /*
+    val a: Int = 1
+    val a1: Long = 1142314
+    val b: Double = 22.2
+    val b1: Float = 23.1090F
+    val c: Boolean = true
+    val d: Char = 'A'
+    val f: String = "AAAAA"
+    val o: Any = "BBB"
+    println(a)
+    println(a1)
+    println(b)
+    println(b1)
+    println(c)
+    println(d)
+    println(f)
+    println(o)
+     */
+
+    /*
+
+     */
+
+
+
+    //Задание 1
+    /*
+    print("Ваша любимая игра/кино/аниме: ")
+    val name = readln()
+    print("Ваша любимая цифра: ")
+    val num = readln()
+    print("Ваша любимая буква алфавита: ")
+    val bykva = readln()
+    println(name)
+    println(num)
+    println(PI)
+    println(bykva)
+
+     */
+
+    //Задание 2
+    /*
+    println("")
+
+     */
+
+    //Задание 3
+    /*
+    val pi: Double = 3.14
+    val radius: Int = 5
+    println(pi*radius*radius)
+     */
+
+    //Задание 4
+    /*
+    print("Ваше имя: ")
+    val name = readln()
+    print("Ваш год рождения: ")
+    val num = readln().toInt()
+    var num1 = 2025-num
+    println("$name, вам $num1 лет")
+
+     */
+    //Задание 5
+    /*
+    print("Ваше число 1: ")
+    val num = readln().toInt()
+    print("Ваше число 2: ")
+    val num1 = readln().toInt()
+    val plus = num+num1
+    val minys = num-num1
+    val ymnoj = num*num1
+    println("$num + $num1 = $plus")
+    println("$num - $num1 = $minys")
+    println("$num * $num1 = $ymnoj")
+     */
+
+    //Задание 6
+    /*
+    print("Ваше число: ")
+    val num = readln().toInt()
+    val a = num/100
+    val b = (num/10)%10
+    val c = num%10
+    println(a+b+c)
+     */
+
+    //Задание 7
+    /*
+    print("Ваше razdelitel: ")
+    val derty = readln()
+    print("Ваше число1: ")
+    val num1 = readln().toInt()
+    print("Ваше число2: ")
+    val num2 = readln().toInt()
+    print("Ваше число3: ")
+    val num3 = readln().toInt()
+    println("$num1$derty$num2$derty$num3")
+    */
+
+    //Задание 8
+    /*
+    print("Ваше число: ")
+    val num1 = readln().toInt()
+    val num2 = num1+1
+    val num3 = num1+2
+    println("$num1")
+    println("$num2")
+    println("$num3")
+     */
+
+    //Задание 9
+    /*
+    print("Ваше число: ")
+    val num = readln().toInt()
+    val a = num/100
+    val b = (num/10)%10
+    val c = num%10
+    println("$c$b$a")
+     */
+
+    //Задание 10
+    /*
+    val num = readln().toInt()
+    val a = (num%1000)/100
+    println(a)
+     */
+
+
+}
